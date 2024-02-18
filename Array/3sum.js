ca2418f2628477ae9551764b8a11ffcc717e33c1
Rupam <rupammondal564@gmail.com>
2024-02-18 19:11:39 +0530

    3 sum solution done

--- Array/3sum.js
+++ Array/3sum.js
@@ -1,26 +1,31 @@
 var threeSum = function (nums) {
     const arr = [];
-    nums.sort((a , b) => a - b);
-    let i = 0;
-    while (i < nums.length - 2) {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
         let j = i + 1;
         let k = nums.length - 1;
         while (j < k) {
-            if (nums[i] + nums[j] + nums[k] == 0) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
                 const arr1 = [nums[i], nums[j], nums[k]];
                 arr.push(arr1);
                 j++;
                 k--;
-            }
-            else if (nums[i] + nums[j] + nums[k] > 0) {
-                k--;
-            }
-            else {
-                j++;
+                while (j < k && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+                while (j < k && nums[k] == nums[k + 1]) {
+                    k--;
+                }
             }
         }
-        i++;
     }
     return arr;
 };
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
