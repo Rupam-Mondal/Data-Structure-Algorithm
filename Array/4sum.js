7ecea9f2b32ead7833bea02241f26999cebc8135
Rupam <rupammondal564@gmail.com>
2024-02-22 08:14:20 +0530

    4sum question is solved

--- Array/4sum.js
+++ Array/4sum.js
@@ -1,33 +1,30 @@
 var fourSum = function (nums, target) {
-    if (nums.length < 4) {
-        return [];
-    }
-    const arr = [];
     nums.sort((a, b) => a - b);
-    let i = 0;
-    let k = nums.length - 1;
-    while (i < k) {
-        let j = i + 1;
-        let l = k - 1;
-        while (j < l) {
-            let sum = nums[i] + nums[j] + nums[l] + nums[k];
-            if (sum > target) {
-                l--;
-            }
-            else if (sum < target) {
-                j++;
-            }
-            else {
-                const arr1 = [nums[i], nums[j], nums[l], nums[k]];
-                arr.push(arr1);
-                j++;
-                l--;
-                while (j < l && nums[j - 1] == nums[j]) j++;
-                while (j < l && nums[l] == nums[l + 1]) l--;
+    const arr = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (i != j - 1 && nums[j] == nums[j - 1]) continue;
+            let k = j + 1;
+            let l = nums.length - 1;
+            while (k < l) {
+                let sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if (sum > target) {
+                    l--;
+                }
+                else if (sum < target) {
+                    k++;
+                }
+                else {
+                    const arr1 = [nums[i], nums[j], nums[k], nums[l]];
+                    arr.push(arr1);
+                    k++;
+                    l--;
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                }
             }
         }
-        i++;
-        k--;
     }
     return arr;
 };
