529935e06bb2c20e686882b1b0698476102cb579
Rupam <rupammondal564@gmail.com>
2024-03-05 20:15:07 +0530

    use case of selction sort

--- 4.sorting/selection_sort.js
+++ 4.sorting/selection_sort.js
@@ -1,3 +1,14 @@
+/**
+ * in worst case there will be n - 1 swaps and approx n^2 comparisn
+ * 
+ * for sorting data where writing is a heavy operation ex :- sorting data in har disk
+ * we use selection sort
+ * 
+ * suppose you have 10 file and you wanna sort wrt data then we have to just do approx n swap
+ * so it is not bad in all cases
+ */
+
+
 function selection(arr) {
     for(let i = 0 ; i < arr.length - 1 ; i++){
         let min_index = i;
