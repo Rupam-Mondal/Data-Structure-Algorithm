797b2beae23b7b35596e94d3d9ae56088d27f442
Rupam <rupammondal564@gmail.com>
2024-03-05 20:20:28 +0530

    note of sorting

--- 4.sorting/selection_sort.js
+++ 4.sorting/selection_sort.js
@@ -1,12 +1,14 @@
 /**
  * in worst case there will be n - 1 swaps and approx n^2 comparisn
  * 
- * for sorting data where writing is a heavy operation ex :- sorting data in har disk
+ * for sorting data where writing is a heavy operation ex :- sorting data in hard disk
  * we use selection sort
  * 
  * suppose you have 10 file and you wanna sort wrt data then we have to just do approx n swap
  * so it is not bad in all cases
  */
+
+
 
 
 function selection(arr) {
