55ed6fd0b25035613a238786a75ff0c36178e2ca
Rupam <rupammondal564@gmail.com>
2024-04-09 16:22:47 +0530

    update

--- 6.Dynamic_programming/best_time_buyi.js
+++ 6.Dynamic_programming/best_time_buyi.js
@@ -1,5 +1,5 @@
 var maxProfit = function (prices) {
-    let profit = 0; 2
+    let profit = 0;
     let buy = prices[0];
     for (let i = 1; i < prices.length; i++) {
         profit = Math.max(profit, prices[i] - buy);
